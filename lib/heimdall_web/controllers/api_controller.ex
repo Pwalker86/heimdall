fb3a6bc714bf9266d41f295962ec5975737f7bb4
Phil Walker <phil1586@gmail.com>
2018-01-24 20:07:08 -0700

    initial solve

--- lib/heimdall_web/controllers/api_controller.ex
+++ lib/heimdall_web/controllers/api_controller.ex
@@ -1,6 +1,7 @@
 defmodule HeimdallWeb.ApiController do
   use HeimdallWeb, :controller
   import Plug.Conn
+  require Integer
 
 
   # this route takes one upc, and returns the upc with the check digit added
@@ -13,8 +14,7 @@ defmodule HeimdallWeb.ApiController do
   # this route takes a comma separated list and should add a check digit to each element
   # http://0.0.0.0:4000/api/add_a_bunch_of_check_digits/12345,233454,34341432
   def add_a_bunch_of_check_digits(conn, params) do
-    check_digits_with_upc = String.split(params.upcs, ",")
-    |> tl
+    check_digits_with_upc = String.split(params["upcs"], ",")
     |> Enum.map((fn upc -> _calculate_check_digit(upc) end))
 
     _send_json(conn, 200, check_digits_with_upc)
@@ -23,7 +23,45 @@ defmodule HeimdallWeb.ApiController do
   # these are private methods
   defp _calculate_check_digit(upc) do
     #this is where your code to calculate the check digit should go
-    upc
+    odd_positions = String.graphemes(upc)
+    |> Enum.with_index(1)
+    |> Enum.map(fn(x) -> _get_odd_positions(x) end )
+    |> Enum.filter(fn(x) -> x != "" end )
+    |> Enum.map(fn(x) -> String.to_integer(x) end )
+    |> Enum.sum
+
+    even_positions = String.graphemes(upc)
+    |> Enum.with_index(1)
+    |> Enum.map(fn(x) -> _get_even_positions(x) end )
+    |> Enum.filter(fn(x) -> x != "" end )
+    |> Enum.map(fn(x) -> String.to_integer(x) end )
+    |> Enum.sum
+
+    odd_positions = odd_positions * 3
+    sum = even_positions + odd_positions
+    remainder = rem(sum, 10)
+    
+    check_digit = if remainder === 0 do
+      0
+    else
+      10 - remainder
+    end
+
+    upc <> Integer.to_string(check_digit)
+  end
+
+  defp _get_odd_positions(tuple) do
+    case Integer.is_odd(elem(tuple, 1)) do
+      true -> elem(tuple, 0)
+      _ -> ""
+    end
+  end
+
+  defp _get_even_positions(tuple) do
+    case Integer.is_even(elem(tuple, 1)) do
+      true -> elem(tuple, 0)
+      _ -> ""
+    end
   end
 
   # this is a thing to format your responses and return json to the client
